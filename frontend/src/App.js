fff9632f4405b2bf34c6e9c20ca3bb56e2ed4fde
Evan <77968134+Ivanjacob@users.noreply.github.com>
2023-09-03 23:37:32 +0300

    forth commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import PrivateRoute from './utils/PrivateRoute';
+
+import Dashboard from './views/Dashboard';
+import Homepage from './views/Homepage';
+import Loginpage from './views/Loginpage';
+import Registerpage from './views/Registerpage';
+import Navbar from './views/Navbar';
+
 
 function App() {
   return (
-    <div>App</div>
+    <Router>
+      <AuthProvider>
+        < Navbar/>
+        <Routes>
+          <Route element={<PrivateRoute/>}>
+            <Route element={Dashboard} path="/dashboard" exact/>
+          </Route>
+          <Route element={Loginpage} path="/login"/>
+          <Route element={Registerpage} path="/register" exact/>
+          <Route element={Homepage} path="/" exact/>
+        </Routes>
+        
+      </AuthProvider>
+    </Router>
   )
 }
 
